508b464b5f4c2ccdd1b85d63a01c43e5322385e5
Shane Mc Cormack <shanemcc@gmail.com>
2021-12-01 19:25:00 +0000

    Day 1 tidying. Don't actually need the whole array sum, just check the difference between first/last value in the window.

--- 1/run.php
+++ 1/run.php
@@ -3,10 +3,11 @@
 	require_once(dirname(__FILE__) . '/../common/common.php');
 	$input = getInputLines();
 
-	function calc($input, $size = 3) {
+	function checkIncrements($input, $size = 3) {
 		$inc = 0;
-		for ($i = $size; $i <= count($input); $i++) {
-			if (array_sum(array_slice($input, $i - 1 - $size, $size)) < array_sum(array_slice($input, $i - $size, $size))) {
+		for ($i = $size; $i < count($input); $i++) {
+			// if (array_sum(array_slice($input, $i - 1 - $size, $size)) < array_sum(array_slice($input, $i - $size, $size))) {
+			if ($input[$i - $size] < $input[$i]) {
 				$inc++;
 			}
 		}
@@ -14,8 +15,8 @@
 	}
 
 
-	$part1 = calc($input, 1);
+	$part1 = checkIncrements($input, 1);
 	echo 'Part 1: ', $part1, "\n";
 
-	$part2 = calc($input, 3);
+	$part2 = checkIncrements($input, 3);
 	echo 'Part 2: ', $part2, "\n";
