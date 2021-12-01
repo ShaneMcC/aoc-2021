f81121c310a85e73f77469aa140f50156f1f4101
Shane Mc Cormack <shanemcc@gmail.com>
2021-12-01 17:32:16 +0000

    Boilerplate.

---
+++ docker.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+BASEIMAGE=shanemcc/aoc-2020-05
+BASEDOCKERFILE="Dockerfile"
+
+IMAGE=${BASEIMAGE}
+DOCKERFILE=${BASEDOCKERFILE}
+FORCEBUILD="0";
+SHELL="0";
+
+while true; do
+	case "$1" in
+		--php74|--php8|--php8-jit)
+			IMAGE=${BASEIMAGE}-${1/--/}
+			DOCKERFILE=${BASEDOCKERFILE}-${1/--/}
+			;;
+		--build)
+			FORCEBUILD="1";
+			;;
+		--shell)
+			SHELL="1";
+			;;
+		*)
+			break;
+			;;
+	esac
+	shift
+done
+
+docker image inspect $IMAGE >/dev/null 2>&1
+if [ $? -ne 0 -o ${FORCEBUILD} = "1" ]; then
+	echo "One time setup: building docker image ${IMAGE}..."
+	cd docker
+	docker build . -t $IMAGE --file ${DOCKERFILE}
+	cd ..
+	echo "Image build complete."
+fi
+
+if [ "${SHELL}" = "1" ]; then
+	docker run --rm -it -v $(pwd):/code $IMAGE bash
+else
+	docker run --rm -it -v $(pwd):/code $IMAGE /entrypoint.sh $@
+fi;
