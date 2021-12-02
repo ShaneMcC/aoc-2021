b7d85f11a88f7692af695da3d354c5208a90d2bb
Shane Mc Cormack <shanemcc@gmail.com>
2021-12-02 10:21:25 +0000

    Day 2. Part 2. 42138

--- 2/run.php
+++ 2/run.php
@@ -4,6 +4,7 @@
 	$input = getInputLines();
 
 	$x = $y = 0;
+	$x2 = $y2 = $aim = 0;
 
 	$directions = ['forward' => [1, 0],
 	               'up' => [0, -1],
@@ -12,9 +13,22 @@
 
 	foreach ($input as $inst) {
 		$bits = explode(' ', $inst);
+
+		// Part 1
 		$dir = $directions[$bits[0]];
 		$x += $dir[0] * $bits[1];
 		$y += $dir[1] * $bits[1];
+
+		// Part 2
+		if ($bits[0] == 'down') {
+			$aim += $bits[1];
+		} else if ($bits[0] == 'up') {
+			$aim -= $bits[1];
+		} else if ($bits[0] == 'forward') {
+			$x2 += $bits[1];
+			$y2 += $aim * $bits[1];
+		}
 	}
 
 	echo 'Part 1: ', $x * $y, "\n";
+	echo 'Part 2: ', $x2 * $y2, "\n";
