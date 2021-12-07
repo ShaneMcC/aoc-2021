e441bd2959e6e5fc463348b318de96786cae2cdf
Shane Mc Cormack <shanemcc@gmail.com>
2021-12-07 22:45:23 +0000

    Minor tidying.

--- 5/run.php
+++ 5/run.php
@@ -5,66 +5,66 @@
 	$input = [];
 	foreach (getInputLines() as $in) {
 		if (preg_match('/([0-9]+),([0-9]+) -> ([0-9]+),([0-9]+)/', $in, $m)) {
-			[$_, $x1, $y1, $x2, $y2] = $m;
-			$inputs[] = [$x1, $y1, $x2, $y2];
+			[$_, $startX, $startY, $endX, $endY] = $m;
+			$inputs[] = [$startX, $startY, $endX, $endY];
 		}
+	}
+
+	function countCrossingPoints($map) {
+		$count = 0;
+		foreach ($map as $row) {
+			foreach ($row as $point) {
+				if ($point > 1) {
+					$count++;
+				}
+			}
+		}
+		return $count;
 	}
 
 	$map = [];
 
 	foreach ($input as $in) {
-		[$x1, $y1, $x2, $y2] = $in;
+		[$startX, $startY, $endX, $endY] = $in;
 
-		if ($x1 == $x2) {
-			$dir = $y1 > $y2 ? -1 : 1;
-			$y = $y1;
+		if ($startX == $endX) {
+			$dir = $startY > $endY ? -1 : 1;
+			$y = $startY;
 			do {
-				if (!isset($map[$y][$x1])) { $map[$y][$x1] = 0; }
-				$map[$y][$x1]++;
-				if ($y == $y2) { break; }
+				if (!isset($map[$y][$startX])) { $map[$y][$startX] = 0; }
+				$map[$y][$startX]++;
+				if ($y == $endY) { break; }
 			} while ($y += $dir);
-		} else if ($y1 == $y2) {
-			$dir = $x1 > $x2 ? -1 : 1;
-			$x = $x1;
+		} else if ($startY == $endY) {
+			$dir = $startX > $endX ? -1 : 1;
+			$x = $startX;
 			do {
-				if (!isset($map[$y1][$x])) { $map[$y1][$x] = 0; }
-				$map[$y1][$x]++;
-				if ($x == $x2) { break; }
+				if (!isset($map[$startY][$x])) { $map[$startY][$x] = 0; }
+				$map[$startY][$x]++;
+				if ($x == $endX) { break; }
 			} while ($x += $dir);
 		}
 	}
 
-	$part1 = 0;
-	foreach ($map as $row) {
-		foreach ($row as $point) {
-			if ($point > 1) { $part1++; }
-		}
-	}
-
+	$part1 = countCrossingPoints($map);
 	echo 'Part 1: ', $part1, "\n";
 
 	foreach ($input as $in) {
-		[$x1, $y1, $x2, $y2] = $in;
-		if ($x1 != $x2 && $y1 != $y2) {
-			$xdir = $x1 > $x2 ? -1 : 1;
-			$x = $x1;
-			$ydir = $y1 > $y2 ? -1 : 1;
-			$y = $y1;
+		[$startX, $startY, $endX, $endY] = $in;
+		if ($startX != $endX && $startY != $endY) {
+			$xdir = $startX > $endX ? -1 : 1;
+			$x = $startX;
+			$ydir = $startY > $endY ? -1 : 1;
+			$y = $startY;
 			while (true) {
 				if (!isset($map[$y][$x])) { $map[$y][$x] = 0; }
 				$map[$y][$x]++;
-				if ($x == $x2 && $y == $y2) { break; }
+				if ($x == $endX && $y == $endY) { break; }
 				$x += $xdir;
 				$y += $ydir;
 			};
 		}
 	}
 
-	$part2 = 0;
-	foreach ($map as $row) {
-		foreach ($row as $point) {
-			if ($point > 1) { $part2++; }
-		}
-	}
-
+	$part2 = countCrossingPoints($map);
 	echo 'Part 2: ', $part2, "\n";
