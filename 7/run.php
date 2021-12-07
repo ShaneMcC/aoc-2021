a8e8a15503c9328dd1bb63e841f1eb1fa3ad2efb
Shane Mc Cormack <shanemcc@gmail.com>
2021-12-07 20:54:34 +0000

    More improvement in speed.

--- 7/run.php
+++ 7/run.php
@@ -3,20 +3,20 @@
 	require_once(dirname(__FILE__) . '/../common/common.php');
 	$input = explode(',', getInputLine());
 
-	function checkPosition($input, $pos) {
+	function checkPosition($input, $pos, $max = PHP_INT_MAX, $expensive = false) {
 		$cost = 0;
-		$expensiveCost = 0;
 		foreach ($input as $in) {
 			$diff = abs($in - $pos);
-			$cost += $diff;
-			$expensiveCost += array_sum(range(0, $diff));
+			$cost += $expensive ? array_sum(range(0, $diff)) : $diff;
+			if ($cost > $max) { return $max; }
 		}
-		return [$cost, $expensiveCost];
+		return $cost;
 	}
 
 	$bestCost1 = $bestPos1 = $bestCost2 = $bestPos2 = PHP_INT_MAX;
 	for ($i = 0; $i < max($input); $i++) {
-		[$cost1, $cost2] = checkPosition($input, $i);
+		$cost1 = checkPosition($input, $i, $bestCost1);
+		$cost2 = checkPosition($input, $i, $bestCost2, true);
 
 		if ($cost1 < $bestCost1) {
 			$bestCost1 = $cost1;
