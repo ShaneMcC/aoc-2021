c3bcfe54d39bef0fab11e42c4500ba05c32c5fee
Shane Mc Cormack <shanemcc@gmail.com>
2021-12-07 20:37:16 +0000

    Day 6. Part 2. 57731.

--- 6/run.php
+++ 6/run.php
@@ -3,17 +3,34 @@
 	require_once(dirname(__FILE__) . '/../common/common.php');
 	$input = explode(',', getInputLine());
 
-	$fish = $input;
-
-	for ($day = 1; $day <= 80; $day++) {
-		for ($i = 0; $i < count($fish); $i++) {
-			$fish[$i]--;
-			if ($fish[$i] < 0) {
-				$fish[$i] = 6;
-				$fish[] = 9;
-			}
-		}
+	$fish = array_fill(0, 10, 0);
+	foreach ($input as $f) {
+		$fish[$f]++;
 	}
 
-	$part1 = count($fish);
-	echo 'Part 1: ', $part1, "\n";
+	$day = 1;
+	do {
+		$newFish = [];
+
+		// Reset our counter.
+		$fish[7] += $fish[0];
+
+		// Add new Fish
+		$fish[9] += $fish[0];
+
+		// Decrease all fish values.
+		for ($i = 0; $i < count($fish); $i++) {
+			$fish[$i] = isset($fish[$i + 1]) ? $fish[$i + 1] : 0;
+		}
+
+		if ($day == 80) {
+			$part1 = array_sum(array_values($fish));
+			echo 'Part 1: ', $part1, "\n";
+		}
+
+		if ($day == 256) {
+			$part2 = array_sum(array_values($fish));
+			echo 'Part 2: ', $part2, "\n";
+			break;
+		}
+	} while ($day++);
