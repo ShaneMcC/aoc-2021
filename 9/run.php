67a0530fff4739d9b8beeb83c327a767c07c09c6
Shane Mc Cormack <shanemcc@gmail.com>
2021-12-09 05:24:02 +0000

    Day 8. Part 2. 1146.

--- 9/run.php
+++ 9/run.php
@@ -1,9 +1,32 @@
 #!/usr/bin/php
 <?php
 	require_once(dirname(__FILE__) . '/../common/common.php');
+	require_once(dirname(__FILE__) . '/../common/pathfinder.php');
 	$map = getInputMap();
 
+	function getBasinCells($map, $x, $y, $known = []) {
+		$adjacent = [];
+
+		if (isset($map[$y - 1][$x])) { $adjacent[] = [$y - 1, $x]; }
+		if (isset($map[$y + 1][$x])) { $adjacent[] = [$y + 1, $x]; }
+		if (isset($map[$y][$x - 1])) { $adjacent[] = [$y, $x - 1]; }
+		if (isset($map[$y][$x + 1])) { $adjacent[] = [$y, $x + 1]; }
+
+
+		$known[] = [$x, $y];
+		foreach ($adjacent as $a) {
+			[$ay, $ax] = $a;
+			if ($map[$ay][$ax] == 9) { continue; }
+			if (!in_array([$ax, $ay], $known)) {
+				$known = getBasinCells($map, $ax, $ay, $known);
+			}
+		}
+
+		return $known;
+	}
+
 	$part1 = 0;
+	$basins = [];
 	foreach (cells($map) as $c) {
 		[$x, $y, $cell] = $c;
 
@@ -23,10 +46,14 @@
 
 		if ($lowPoint) {
 			$part1 += 1 + $cell;
+
+			$cells = getBasinCells($map, $x, $y);
+			$basins[] = count($cells);
 		}
 	}
 
 	echo 'Part 1: ', $part1, "\n";
 
-	// $part2 = -1;
-	// echo 'Part 2: ', $part2, "\n";
+	rsort($basins);
+	$part2 = $basins[0] * $basins[1] * $basins[2];
+	echo 'Part 2: ', $part2, "\n";
