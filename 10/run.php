8ad4a73ee776a4e29742669c1453344efb320569
Shane Mc Cormack <shanemcc@gmail.com>
2021-12-10 05:16:44 +0000

    Day 10. Part 2. 2235.

--- 10/run.php
+++ 10/run.php
@@ -3,14 +3,14 @@
 	require_once(dirname(__FILE__) . '/../common/common.php');
 	$input = getInputLines();
 
-	$points = [')' => 3, ']' => 57, '}' => 1197, '>' => 25137];
+	$corruptedPoints = [')' => 3, ']' => 57, '}' => 1197, '>' => 25137];
+	$incompletePoints = [')' => 1, ']' => 2, '}' => 3, '>' => 4];
 
 	$part1 = 0;
+	$part2 = [];
 
-	$validLines = [];
 	$corruptedLines = [];
 	$incompleteLines = [];
-
 	foreach ($input as $line) {
 		$bits = str_split($line);
 		$expected = [];
@@ -35,17 +35,23 @@
 					$wanted = empty($expected) ? '' : array_pop($expected);
 					if ($wanted != $bit) {
 						// echo 'Expected ', $wanted, ', but found ', $bit, ' instead.', "\n";
-						$corruptedLines[] = $line;
-						$part1 += $points[$bit];
-						break 2;
+						$part1 += $corruptedPoints[$bit];
+						continue 3;
 					}
 			}
 		}
-		if (empty($expected)) {
-			$validLines[] = $line;
-		} else {
-			$incompleteLines[] = $line;
+
+		$points = 0;
+		foreach (array_reverse($expected) as $bit) {
+			$points *= 5;
+			$points += $incompletePoints[$bit];
 		}
+		// echo 'Complete by adding: ', implode(array_reverse($expected)), ' for ', $points, ' points.', "\n";
+		$part2[] = $points;
 	}
 
+	sort($part2);
+	$part2 = $part2[count($part2) / 2];
+
 	echo 'Part 1: ', $part1, "\n";
+	echo 'Part 2: ', $part2, "\n";
