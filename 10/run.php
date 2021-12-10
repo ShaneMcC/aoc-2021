4d5e4deefc86abc44b0a15333c319981cda66f92
Shane Mc Cormack <shanemcc@gmail.com>
2021-12-10 05:24:26 +0000

    Tidying.

--- 10/run.php
+++ 10/run.php
@@ -6,48 +6,41 @@
 	$corruptedPoints = [')' => 3, ']' => 57, '}' => 1197, '>' => 25137];
 	$incompletePoints = [')' => 1, ']' => 2, '}' => 3, '>' => 4];
 
+	$opposite = ['(' => ')', '[' => ']', '{' => '}', '<' => '>'];
+
 	$part1 = 0;
 	$part2 = [];
 
-	$corruptedLines = [];
-	$incompleteLines = [];
 	foreach ($input as $line) {
 		$bits = str_split($line);
 		$expected = [];
 		foreach ($bits as $bit) {
 			switch ($bit) {
 				case '(':
-					$expected[] = ')';
-					break;
 				case '[':
-					$expected[] = ']';
-					break;
 				case '{':
-					$expected[] = '}';
-					break;
 				case '<':
-					$expected[] = '>';
+					$expected[] = $opposite[$bit];
 					break;
+
 				case ')':
 				case ']':
 				case '}':
 				case '>':
 					$wanted = empty($expected) ? '' : array_pop($expected);
 					if ($wanted != $bit) {
-						// echo 'Expected ', $wanted, ', but found ', $bit, ' instead.', "\n";
 						$part1 += $corruptedPoints[$bit];
 						continue 3;
 					}
 			}
 		}
 
-		$points = 0;
+		$linePoints = 0;
 		foreach (array_reverse($expected) as $bit) {
-			$points *= 5;
-			$points += $incompletePoints[$bit];
+			$linePoints *= 5;
+			$linePoints += $incompletePoints[$bit];
 		}
-		// echo 'Complete by adding: ', implode(array_reverse($expected)), ' for ', $points, ' points.', "\n";
-		$part2[] = $points;
+		$part2[] = $linePoints;
 	}
 
 	sort($part2);
