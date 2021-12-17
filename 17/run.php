18ae354a1bbb7db49fc2c27457521bdff08bff11
Shane Mc Cormack <shanemcc@gmail.com>
2021-12-17 05:43:43 +0000

    Day 17. Part 2. 1862. Still not a good solution.

--- 17/run.php
+++ 17/run.php
@@ -8,23 +8,16 @@
 
 	$target = ['x' => ['start' => $startX, 'end' => $endX], 'y' => ['start' => $startY, 'end' => $endY]];
 
-
-	function step($probe) {
-		$probe['x'] += $probe['vx'];
-		$probe['y'] += $probe['vy'];
-
-		$probe['vx'] += $probe['vx'] > 0 ? -1 : 1;
-		$probe['vy'] += -1;
-
-		return $probe;
-	}
-
 	function testProbe($target, $vx, $vy) {
 		$probe = ['x' => 0, 'y' => 0, 'vx' => $vx, 'vy' => $vy];
 
 		$highestY = 0;
 		while (true) {
-			$probe = step($probe);
+			$probe['x'] += $probe['vx'];
+			$probe['y'] += $probe['vy'];
+			if ($probe['vx'] != 0) { $probe['vx'] += $probe['vx'] > 0 ? -1 : 1; }
+			$probe['vy'] += -1;
+
 			$highestY = max($highestY, $probe['y']);
 
 			$inX = $probe['x'] >= $target['x']['start'] && $probe['x'] <= $target['x']['end'];
@@ -34,25 +27,28 @@
 				return [true, $highestY];
 			}
 
-			if ($probe['y'] <= min($target['y']['start'], $target['y']['end'])) {
-				return [false, $highestY];
+			if ($probe['y'] < min($target['y']['start'], $target['y']['end'])) {
+				return [false, -1];
+			}
+
+			if ($probe['vx'] == 0 && !$inX) {
+				return [false, -1];
 			}
 		}
 	}
 
-
+	$valid = 0;
 	$highestY = 0;
 	for ($vx = 0; $vx <= $target['x']['end']; $vx++) {
-		for ($vy = 0; $vy <= 200; $vy++) {
+		for ($vy = $target['y']['start']; $vy <= 300; $vy++) {
 			[$result, $testHighestY] = testProbe($target, $vx, $vy);
 			if ($result) {
-				echo json_encode([$vx, $vy, $testHighestY, $highestY]), "\n";
+				// echo json_encode([$vx, $vy, $testHighestY, $highestY]), "\n";
 				$highestY = max($highestY, $testHighestY);
+				$valid++;
 			}
 		}
 	}
 
 	echo 'Part 1: ', $highestY, "\n";
-
-	// $part2 = -1;
-	// echo 'Part 2: ', $part2, "\n";
+	echo 'Part 2: ', $valid, "\n";
