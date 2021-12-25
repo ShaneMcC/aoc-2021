2addc307e5f2a28e572f257f233f8e90e291995e
Shane Mc Cormack <shanemcc@gmail.com>
2021-12-25 05:14:20 +0000

    Day 25. Part 1 (and thus 2). 107/98.

---
+++ 25/run.php
@@ -0,0 +1,36 @@
+#!/usr/bin/php
+<?php
+	require_once(dirname(__FILE__) . '/../common/common.php');
+	$map = getInputMap();
+
+	$count = 0;
+	while (true) {
+		$startMap = $map;
+
+		$newMap = $map;
+		foreach (cells($map) as [$x, $y, $cell]) {
+			if ($cell == '>') {
+				if ($map[$y][($x + 1) % count($map[$y])] == '.') {
+					$newMap[$y][$x] = '.';
+					$newMap[$y][($x + 1) % count($map[$y])] = $cell;
+				}
+			}
+		}
+
+		$map = $newMap;
+		foreach (cells($map) as [$x, $y, $cell]) {
+			if ($cell == 'v') {
+				if ($map[($y + 1) % count($map)][$x] == '.') {
+					$newMap[$y][$x] = '.';
+					$newMap[($y + 1) % count($map)][$x] = $cell;
+				}
+			}
+		}
+		$map = $newMap;
+		$count++;
+		if ($startMap == $map) {
+			break;
+		}
+	}
+
+	echo 'Part 1: ', $count, "\n";
